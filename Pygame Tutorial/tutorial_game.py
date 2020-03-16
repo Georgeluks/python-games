296bce6df0aede73929b3509a823583500998344
George Lucas Ebertz Prado <george.lucasebertz98@gmail.com>
2020-03-16 16:45:34 -0300

    adding files via upload
    just the first two folders i have in my notebook to continue working on my desktop

---
+++ Pygame Tutorial/tutorial_game.py
@@ -0,0 +1,58 @@
+#Tutorial 2D game
+
+import pygame 
+
+pygame.init()
+
+
+win = pygame.display.set_mode((500, 500))
+#this line creates a window of 500W and 500h
+
+pygame.display.set_caption("First Game")
+#display the game name in the windows display
+
+#Moving the Character
+
+#charachter attributtes
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+is_jump = False
+jump_count = 10
+
+while run:
+    pygame.time.delay(100) #this will delay the game the given amount o milliseconds, in this case, 0.1 seconds
+
+    for event in pygame.event.get(): #This will loop through a list of any keyboard or mouse events. 
+        if event.type == pygame.QUIT: #checks if the red button in the corner of the windows is clicked
+            run = False #Ends the game loop
+
+    keys = pygame.key.get_pressed() #this will give us a dictionary where each key has a vlaue of 1 or 0, where 1 is pressed and 0 isnt
+
+    #we can check if a key is pressed like this
+
+    if keys[pygame.K_LEFT] and x > vel: #setup boundaries so the top left position of the character is greater than our vel so we never move off screen
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < 500 - vel - width: #setup boundaries so the top right corner of our character is less than the screen width - its width
+        x += vel
+
+    if keys[pygame.K_UP] and y > vel: #same aplly for the Y coordinate
+        y -= vel
+
+    if keys[pygame.K_DOWN] and y < 500 - vel - height:
+       y += vel
+    
+    if keys[pygame.K_SPACE]:
+        is_jump = True
+
+    win.fill((0,0,0)) #fills the screen with black so we dont keep track of the character last known position
+    pygame.draw.rect(win, (255, 0 , 0), (x, y, width, height)) #This takes: windows/surface, color, rect
+    pygame.display.update() #this updates the screen so we can see our rectangle
+
+pygame.quit() #if we exit the loop this will execute and close our game
+
---
+++ Snake/snakeGame.py
@@ -0,0 +1,53 @@
+#Snake Game
+
+import math
+import random
+import pygame
+import tkinter as tk 
+from tkinter import messagebox
+
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self, start,dirnx=1,dirny=0,color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def draw_grid(w, rows, surface):
+    pass
+
+def redraw_window(surface):
+    pass
+
+def random_snack(rows, item):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
+
+main()
